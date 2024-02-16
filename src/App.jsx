eff4ceea15c5692119cecc50ba8d29eda0105279
kimsohyeon <aotoyae@gmail.com>
2024-02-17 04:25:29 +0900

    delete: 주석 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import GlobalStyle from "GlobalStyle";
 import Router from "shared/Router";
-// import styled from "styled-components";
-// import background from "assets/background.jpg";
-
-// const StWrap = styled.div`
-//   background-image: url(${background});
-// `;
 
 function App() {
   return (
