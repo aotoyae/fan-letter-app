e40e14f43bb7b7fe268c5dd165bfcddd38ffd97c
kimsohyeon <aotoyae@gmail.com>
2024-02-17 04:18:34 +0900

    Fix: props error 해결

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -11,7 +11,7 @@ export default function Router() {
 
   return (
     <BrowserRouter>
-      <Layout activeMember={activeMember} setActiveMember={setActiveMember}>
+      <Layout $activeMember={activeMember} $setActiveMember={setActiveMember}>
         <Routes>
           <Route
             path="/"
